864da4d1f4176cf6955b07e04bd1374e52793769
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-06-02 23:11:35 +0900

    add error handling plicitly

--- http/ping.go
+++ http/ping.go
@@ -7,5 +7,5 @@ import (
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("pong"))
+	_, _ = w.Write([]byte("pong"))
 }
